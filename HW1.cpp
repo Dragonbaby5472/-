0f8e214f25092265ddc38df5353642ee6d84442f
Dragonbaby5472 <86150451+Dragonbaby5472@users.noreply.github.com>
2021-12-24 14:27:33 +0800

    Update HW1.cpp

--- HW1.cpp
+++ HW1.cpp
@@ -3,10 +3,10 @@
 int main() {
 	std::cout << "------------------------------------------\n";
 	std::cout << "|  Name:       Leo Tsai                  |\n";
-	std::cout << "|  Student id: E24106288                 |\n";
+	std::cout << "|  Student id: E12345678                 |\n";
 	std::cout << "|  Cellphone:  0912345678                |\n";
 	std::cout << "|  Hobby:      Writing HW                |\n";
-	std::cout << "|  Email:      E24106288@gs.ncku.edu.tw  |\n";
+	std::cout << "|  Email:      E12345678@gs.ncku.edu.tw  |\n";
 	std::cout << "------------------------------------------\n";
 	return 0;
 }
